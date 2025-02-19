a1fc47eeb89d6d6b39ec5b5f499105974f2c0bec
UtasiB <utasibalazs@turr.hu>
2025-02-19 08:23:15 +0100

    menü

--- Public/Aprohirdetes/src/app/components/navbar/navbar.component.ts
+++ Public/Aprohirdetes/src/app/components/navbar/navbar.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -14,41 +15,42 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent implements OnInit{
-  isLoggedIn: boolean = false;
+  isLoggedIn$: Observable<boolean>; // Observable to track login state
+  navItems: any[] = [];
+
   constructor(private router: Router, private authService: AuthService) {
-    this.isLoggedIn = this.getToken() !== null;
-   }
-  navItems: any [] = [];
-  
-  ngOnInit(): void {
-    this.navbarLoad();
+    // Subscribe to the login status observable from the AuthService
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
   }
 
-  navbarLoad(){
-    if(this.isLoggedIn == true){
+  ngOnInit(): void {
+    // Update navItems based on login status when the component initializes
+    this.updateNavItems();
+    
+    // Subscribe to the login status observable
+    this.isLoggedIn$.subscribe(isLoggedIn => {
+      this.updateNavItems();
+    });
+  }
+
+  // This method updates navItems based on login status
+  updateNavItems() {
+    if (this.authService.getToken()) {
       this.navItems = [
         { label: 'Hirdetések', url: '/ads' },
         { label: 'Hirdetések kezelése', url: '/adsHandle' },
-        { label: 'Kijelentkezés', url : '/logout'}
-      ]
-    }
-    else{
+        { label: 'Kijelentkezés', url: '/logout', action: () => this.logout() }
+      ];
+    } else {
       this.navItems = [
         { label: 'Bejelentkezés', url: '/login' },
         { label: 'Regisztráció', url: '/register' }
-      ]
+      ];
     }
-  }
-  getToken(): string | null {
-    return localStorage.getItem('aprohirdetes');
   }
 
   logout() {
-    console.log('logout');
-    localStorage.removeItem('aprohirdetes');
-    localStorage.removeItem('user');
-    localStorage.removeItem('loggedUserId');
+    this.authService.logout();
     this.router.navigate(['/login']);
   }
-
 }
